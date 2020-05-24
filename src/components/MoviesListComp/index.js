cf60fb119303d707baef05a4dcdc74e130356853
MohammedYehia <m-ya@windowslive.com>
2020-05-24 20:03:11 +0300

    refactor list

--- src/components/MoviesListComp/index.js
+++ src/components/MoviesListComp/index.js
@@ -3,25 +3,28 @@ import Card from '../Card';
 
 import './style.css';
 
+const createCard = (movie, deleteMovie) => {
+  const { id, movieName, poster, rate, imdbLink, details, likes } = movie;
+  return (
+    <li key={id}>
+      <Card
+        id={id}
+        poster={poster}
+        movieName={movieName}
+        imdbLink={imdbLink}
+        details={details}
+        likes={likes}
+        rate={rate}
+        deleteMovie={deleteMovie}
+      />
+    </li>
+  );
+}
+
+
 const MoviesListComp = ({ movies, deleteMovie }) => (
   <ul>
-    {movies.map(movie => {
-      const { id, movieName, poster, rate, imdbLink, details, likes } = movie;
-      return (
-        <li key={id}>
-          <Card
-            id={id}
-            poster={poster}
-            movieName={movieName}
-            imdbLink={imdbLink}
-            details={details}
-            likes={likes}
-            rate={rate}
-            deleteMovie={deleteMovie}
-          />
-        </li>
-      );
-    })}
+    {movies.map(movie => createCard(movie, deleteMovie))}
   </ul>
 )
 
